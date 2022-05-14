de246d70c46776a01320bafbd7804ef384f5d174
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-05-14 13:26:38 -0500

    Modelado del modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 
 import React from 'react';
 import Content from './components/panel/Content';
+import Modal from './components/layouts/modal';
+
 const App = () => {
 
   return (
-    <Content/>
+    <>
+      <Content/>
+      <Modal />
+    </>
   )
 }
 
---
+++ src/components/layouts/modal.jsx
@@ -0,0 +1,14 @@
+const Modal = () => {
+    return (
+        <>
+            <p>Modal</p>
+            <div className="modalBackground">
+                <div className="modalContainer">
+                    <button>X</button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Modal;
